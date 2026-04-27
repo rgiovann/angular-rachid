ee0dd006882dcd0c02d196c64c9df0f96eeb74ff
Giovani L. Rozza <giovanni.rozza@gmail.com>
2026-04-27 19:32:19 -0300

    app transformado em PWA, criado webmanifest, ngsw-config.json ngsw.json, alterado jsons para compatibilizar dependencias com serviceWorker

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,15 @@ import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
-import { LOCALE_ID } from '@angular/core';
+import { LOCALE_ID, isDevMode } from '@angular/core';
 import { provideNativeDateAdapter } from '@angular/material/core';
+import { provideServiceWorker } from '@angular/service-worker';
 
 registerLocaleData(localePt);
 
 bootstrapApplication(App, {
-  providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' }, provideNativeDateAdapter()],
+  providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' }, provideNativeDateAdapter(), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })],
 }).catch((err) => console.error(err));
